4657ce6fd7d2ef82c22ad5f6fa2a47979b276495
kevinGo <kevinGo@gmail.com>
2026-04-01 12:06:09 -0500

    agregar

--- Register_student.py
+++ Register_student.py
@@ -3,8 +3,12 @@ def Add_student(register):
     while Add == 'yes':
         id = len(register) + 1
         Name = str(input('Name the student: '))
+        if Name.isnumeric():
+            print('name invalid')
         Years = int(input('Years the student: '))
         Course_program = str(input('Which course or program?: '))
+        if Course_program.isnumeric():
+            print('Course_program inavlid')
         State = input('Assed / deactivated: ')
         # Create new dictionary the student
         add = { 'ID': id, 'name': Name, 'years': Years, 'Course_program': Course_program, 'state': State}
